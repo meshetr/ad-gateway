cdae342dcb0f6b69eba07f73e2ba65eaa9315631
ZanHorvat <zhorvat0716@gmail.com>
2021-01-14 23:08:05 +0100

    Init.

---
+++ src/test/java/si/meshetr/ad_gateway/AdGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package si.meshetr.ad_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
